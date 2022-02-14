35bb40d3361d36bd5d2baf2c14aa97139e5f941a
Jaymar Bantay <j.bantay@irri.org>
2022-02-14 20:39:27 +0800

    It works!

--- numeral.js
+++ numeral.js
@@ -1,62 +1,25 @@
 class Numeral {
     convertToRoman(integer) {
+        let levels = [
+            // { val: 1000, str: "M" },
+            // { val: 500, str: "D" },
+            // { val: 100, str: "C" },
+            // { val: 50, str: "L" },
+            // { val: 10, str: "X" },
+            // { val: 5, str: "V" },
+            { val: 1, str: "I" },
+            { val: 0, str: "" }
+        ];
         let result = '';
-        let decrease = 1;
-        while(integer > 0) {
-            let numeralVal = 'I';
-            decrease = 1
 
-            if(integer >= 1000) {
-                decrease = 1000;
-                numeralVal = 'M';
-            }
-            else if(integer >= 900) {
-                decrease = 900;
-                numeralVal = 'DM';
-            }
-            else if(integer >= 500) {
-                decrease = 500;
-                numeralVal = 'D';
-            }
-            else if(integer >= 400) {
-                decrease = 400;
-                numeralVal = 'CD';
-            }
-            else if(integer >= 100) {
-                decrease = 100;
-                numeralVal = 'C';
-            }
-            else if(integer >= 90) {
-                decrease = 90;
-                numeralVal = 'XC';
-            }
-            else if(integer >= 50) {
-                decrease = 50;
-                numeralVal = 'L';
-            }
-            else if(integer >= 40) {
-                decrease = 40;
-                numeralVal = 'XL';
-            }
-            else if(integer >= 10) {
-                decrease = 10;
-                numeralVal = 'X';
-            }
-            else if(integer === 9) {
-                decrease = integer;
-                numeralVal = 'IX';
-            }
-            else if(integer >= 5) {
-                decrease = 5;
-                numeralVal = 'V';
-            }
-            else if(integer === 4) {
-                decrease = integer;
-                numeralVal = 'IV';
-            }
+        for(let i = 0; i < levels.length-1; i++) {
+            let val = levels[i].val
+            let str = levels[i].str
+            while(integer >= val) {
+                result = result.concat(str);
 
-            result = result.concat(numeralVal);
-            integer -= decrease;
+                integer -= val;
+            }
         }
 
         return result;
