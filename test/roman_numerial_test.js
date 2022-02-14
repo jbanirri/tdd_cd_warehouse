c78a05a652c3065b09fd8f6730b6e183b85613c1
Jaymar Bantay <j.bantay@irri.org>
2022-02-14 18:14:35 +0800

    Initial commit.

---
+++ test/roman_numerial_test.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+
+describe("Convert interger to roman numerals", () => {
+    it('convert 1 to I', () => {
+        assert.strictEqual('I', itr.convert(1));
+    })
+})
