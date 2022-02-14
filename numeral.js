77d9d24b77b21dc87bf8b7150a72efab5f8c5dc5
Jaymar Bantay <j.bantay@irri.org>
2022-02-14 18:30:26 +0800

    It works!

--- numeral.js
+++ numeral.js
@@ -1,8 +1,8 @@
 class Numeral {
     convertToRoman(integer) {
-        if(integer===1) return 'I';
-        if(integer===2) return 'II';
-        if(integer===3) return 'III';
+        if(integer<=3) {
+            return "I".repeat(integer)
+        }
     }
 }
 
